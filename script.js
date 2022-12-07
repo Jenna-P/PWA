2d2130b1aeff7f63539401a33213eeaf526151fe
Jenna Petersen <70805517+Jenna-P@users.noreply.github.com>
2022-12-07 14:02:47 +0100

    drag and drop

--- script.js
+++ script.js
@@ -56,8 +56,8 @@ btns.forEach((btn) => {
     });
  // console.log(todo_div);
   
-    // todo_div.addEventListener("dragstart", dragStart);
-    // todo_div.addEventListener("dragend", dragEnd);
+    todo_div.addEventListener("dragstart", dragStart);
+    todo_div.addEventListener("dragend", dragEnd);
   
     document.getElementById("task_input").value = "";
     task_form.classList.remove("active");
@@ -71,3 +71,57 @@ btns.forEach((btn) => {
       btn.parentElement.style.display = "none";
     });
   });
+
+//   drag and drop
+const todos = document.querySelectorAll(".todo");
+const all_status = document.querySelectorAll(".status");
+let draggableTodo = null;
+
+todos.forEach((todo) => {
+  todo.addEventListener("dragstart", dragStart);
+  todo.addEventListener("dragend", dragEnd);
+});
+
+function dragStart() {
+  draggableTodo = this;
+  setTimeout(() => {
+    this.style.display = "none";
+  }, 0);
+  console.log("dragStart");
+}
+
+function dragEnd() {
+  draggableTodo = null;
+  setTimeout(() => {
+    this.style.display = "block";
+  }, 0);
+  console.log("dragEnd");
+}
+
+all_status.forEach((status) => {
+  status.addEventListener("dragover", dragOver);
+  status.addEventListener("dragenter", dragEnter);
+  status.addEventListener("dragleave", dragLeave);
+  status.addEventListener("drop", dragDrop);
+});
+
+function dragOver(e) {
+  e.preventDefault();
+  //   console.log("dragOver");
+}
+
+function dragEnter() {
+  this.style.border = "1px dashed #ccc";
+  console.log("dragEnter");
+}
+
+function dragLeave() {
+  this.style.border = "none";
+  console.log("dragLeave");
+}
+
+function dragDrop() {
+  this.style.border = "none";
+  this.appendChild(draggableTodo);
+  console.log("dropped");
+}
